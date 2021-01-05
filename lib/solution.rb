d2ebf9cd639531d9490decc00ecbfb9f5f1b1dd7
James Sapp <jameseisapp@gmail.com>
2021-01-05 10:04:10 -0600

    commit tree 1f660e5ecb71bbc86b94e60c3b608dbc607a0166

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
+def convert_time(time)
+    new_time = time.split(':', 2)
+    hour = new_time[0].to_i
+    minute = new_time[1].to_i
+end
+
+#binding.pry 
+
+
 def clock_angle(time)
-    # code your solution here
+    convert_time(time)
+    clock = 360
+    hour = 30
+    minute = 6
+    second = 0.1
+
 end
